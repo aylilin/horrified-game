6b289f329dc308e31442fb67c26de8fd700fc464
aylilin <aylinshahrokhi85@gmail.com>
2025-05-27 22:51:04 +0330

    game project started

---
+++ hero.h
@@ -0,0 +1,29 @@
+#ifndef HERO_H
+#define HERO_H
+
+#include <vector>
+#include <string>
+
+class Hero
+{
+protected:
+    std::string name;
+    int actions;
+    std::vector<std::string> items;      //items that hero carries
+    std::string currentLocation;         //where hero is?
+
+public:  
+    Hero(const std::string& name , int actions , std::string& startLocation);
+    virtual void specialAbility() = 0;
+    void move(const std::string& newLocation);
+    void pickup(const std::string& item);
+    void guide();
+    void advance();
+    //void defeat(const std::string& )
+    //special action is just for archeologist
+    
+    std::string get_name() const;
+    int get_actions() const;
+};
+
+#endif
