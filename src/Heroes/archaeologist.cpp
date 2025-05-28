4296fc76c734686afc51b67cb1758dc2e7743976
aylilin <aylinshahrokhi85@gmail.com>
2025-05-29 00:23:57 +0330

    I'm completing this file

---
+++ src/Heroes/archaeologist.cpp
@@ -0,0 +1,26 @@
+#include "archaeologist.h"
+#include <stdexcept>
+
+Archaeologist::Archaeologist(const std::string& name , const std::string& startLocation)
+: Hero(name , 4 , startLocation)
+{
+    if (name.empty())
+    {
+        throw std::invalid_argument("archaeologist's name could not be empty!");
+    }
+
+    if (startLocation.empty())
+    {
+        throw std::invalid_argument("startlocation could not be empty!");
+    }
+}
+
+void Archaeologist::specialAction()
+{
+    if (remainingActions <= 0)
+    {
+        throw std::runtime_error("there is not enough action...");
+    }
+
+    std::vector<std::string> neighborItems;
+}
