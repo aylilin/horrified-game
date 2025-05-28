48df4a9d957243b4ab6b96e995d61f238ca638b5
aylilin <aylinshahrokhi85@gmail.com>
2025-05-28 23:58:37 +0330

    file archaeologist.h created and completed

---
+++ include/Heroes/archaeologist.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "hero.h"
+
+class Archaeologist : public Hero
+{
+public:
+    Archaeologist(const std::string& name , const std::string& startLocation)
+
+    void specialAction() override;
+}
