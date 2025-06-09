f4cabc0cc91db6cb38e1c41b03e7e449943e5b77
aylilin <aylinshahrokhi85@gmail.com>
2025-06-09 23:21:17 +0330

    mayor.cpp added

---
+++ src/Mayor.cpp
@@ -0,0 +1,25 @@
+#include "Heroes/Mayor.h"
+#include <iostream>
+
+Mayor::Mayor(const std::string& name , const std::string& startLocation)
+: Hero(name , 5 , startLocation)
+{
+    if (name.empty())
+    {
+        throw std::invalid_argument("Mayor's name could not be empty!");
+    }
+}
+
+void Mayor::specialAbility()
+{
+    std::cout << name << " has no specialAction!" << std::endl;
+}
+
+void Mayor::manageTownResources()
+{
+    if (remainingActions > 0)
+    {
+        std::cout << "Mayor manages townresources..." << std::endl;
+        remainingActions--;
+    }
+}
