900294f5018fe1a5ffc4c3fa474066c083830b9d
aylilin <aylinshahrokhi85@gmail.com>
2025-06-24 22:51:38 +0330

    some changes

--- include/villager.h
+++ include/villager.h
@@ -8,15 +8,19 @@ private:
     std::string currentLocation;
     std::string safeLocation;
     bool alive;
+    bool guided;
 
 public:
-    Villager(const std::string& name , const std::string& startLocation , const std::string& safeLocation);
+    Villager(const std::string& name , const std::string& safeLocation);
 
     std::string get_name() const;
     std::string get_currentLocation() const;
     std::string get_safeLocation() const;
 
     void set_location(const std::string& newLoc);
+    bool isGuided() const;
+    void set_guided(bool);
+
 
     bool isAlive() const;
     void kill();
