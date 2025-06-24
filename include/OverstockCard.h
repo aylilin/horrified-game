f539b2c720602d491b632a9540e56e0f5f0ea72b
aylilin <aylinshahrokhi85@gmail.com>
2025-06-24 22:39:34 +0330

    another perk card added

---
+++ include/OverstockCard.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../include/perkCard.h"
+
+class OverstockCard : public PerkCard 
+{
+public:
+    std::string get_name() const override;
+    std::string get_description() const override;
+
+    void apply(Hero& currentHero , std::vector<Hero*>&allHeros , std::vector<Monster*>&monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase) override;
+
+};
