fb10f6005a81c8dffa1e0519a0d08aac652486ca
aylilin <aylinshahrokhi85@gmail.com>
2025-06-24 21:55:00 +0330

    a monster card added

---
+++ include/FormerEmployerCard.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "monsterCard.h"
+
+class FormerEmployerCard : public MonsterCard
+{
+public:
+    std::string get_name() const override;
+    std::string get_description() const override;
+    void apply(Map& map, std::vector<Monster*>& monsters, std::vector<Hero*>& heroes, Dice& dice) override;
+};
