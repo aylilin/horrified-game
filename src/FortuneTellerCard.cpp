ef55b2b87607b7ed540578abb9db0a05451d3496
aylilin <aylinshahrokhi85@gmail.com>
2025-06-25 01:00:06 +0330

    another monster card

---
+++ src/FortuneTellerCard.cpp
@@ -0,0 +1,29 @@
+#include "../include/FortuneTellerCard.h"
+#include "../include/villager.h"
+#include "../include/itemBag.h"
+#include <iostream>
+
+std::string FortuneTellerCard::get_name() const
+{
+    return "Fortune Teller";
+}
+
+std::string FortuneTellerCard::get_description() const
+{
+    return "Add 3 items. Place Maleva at Camp. Strike: 1 move + 2 dice (Frenzied Monster)";
+}
+
+void FortuneTellerCard::apply(Map& map, std::vector<Monster*>& monsters, std::vector<Hero*>&, Dice& dice)
+{
+    std::vector<Item> items = map.get_itemBag().drawRandomItems(3);
+    for (const auto& item : items)
+    {
+        map.placeItem(item);
+    }
+
+    Villager* maleva = new Villager("Maleva", "Shop"); // safeLocation = Shop
+    maleva->set_location("Camp");
+    map.addVillager(maleva);
+
+    std::cout << "Maleva appeared at Camp.\n";
+}
