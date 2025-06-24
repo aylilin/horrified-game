030fa06c722b0a8446b74153cb79cd6747959406
aylilin <aylinshahrokhi85@gmail.com>
2025-06-24 22:46:08 +0330

    A small change in line 10

--- include/RepelCard.h
+++ include/RepelCard.h
@@ -7,5 +7,5 @@ public:
     std::string get_name() const override;
     std::string get_description() const override;
 
-    void apply(Hero& currentHero , std::vector<Hero*>&allHeros , std::vector<Monster>&monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase) override;
+    void apply(Hero& currentHero , std::vector<Hero*>&allHeros , std::vector<Monster*>&monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase) override;
 };
