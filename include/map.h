53a4edda86e48e680fa061c93ea0cc746da0568e
aylilin <aylinshahrokhi85@gmail.com>
2025-06-24 22:29:34 +0330

    some funcions related to villagers and heroes added

--- include/map.h
+++ include/map.h
@@ -6,12 +6,18 @@
 #include <set>
 #include "item.h"
 #include "villager.h"
+#include "hero.h"
+#include "monster.h"
+
+class Monster;
+class Hero;
 
 class Map
 {
 public:
     Map();
-
+    ~Map();
+ 
     void addLocation(const std::string& name);
     void connectLocations(const std::string& from , const std::string& to);
     bool areConnected(const std::string& from , const std::string& to) const;
@@ -20,6 +26,8 @@ public:
     void placeItem(const Item& item);
     const std::vector<Item>& get_itemsAt(const std::string& location) const;
     std::vector<Item>& get_itemsAt(const std::string& location); 
+    ItemBag& get_itemBag();
+
 
     //for monster and hero
     void set_characterLocation(const std::string& characterName , const std::string& location);
@@ -27,8 +35,19 @@ public:
 
     //move on the map
     bool moveCharacter(const std::string& characterName , const std::string& toLocation);
-    void addVillager(const Villager& v);
     std::vector<Villager*> get_villagersAt(const std::string& location);
+    std::vector<Hero*> Map::getHeroesAt(const std::string& location);
+
+    void placeHero(Hero* hero, const std::string& location);
+    void placeMonster(Monster* monster, const std::string& location);
+    void moveMonster(Monster* monster, const std::string& newLocation);
+
+
+    std::vector<Villager*> getVillagersAt(const std::string& location);
+    void markVillagerAsGuided(const std::string& name);
+    void displayAvailableVillagers() const;
+    void addVillager(Villager* v);
+
 
     void printMap() const;
     void printItems() const;
@@ -37,5 +56,9 @@ private:
     std::map<std::string , std::set<std::string>> adjacencyList; 
     std::map<std::string , std::vector<Item>> itemsInLocation;    
     std::map<std::string , std::string> characterPositions; 
-    std::vector<Villager> villagers;
+    std::map<std::string, std::vector<Hero*>> heroPositions;
+    std::vector<Villager*> villagers;
+    std::vector<Item> itemsOnMap;
+    ItemBag itemBag;
+
 };
