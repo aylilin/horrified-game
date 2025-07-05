6c92f31ff5a83a0ab05d247ba54bc5b4c7504567
aylilin <aylinshahrokhi85@gmail.com>
2025-07-05 17:23:28 +0330

    class GameController added

--- include/FormOfTheBatCard.h
+++ include/FormOfTheBatCard.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "../include/monsterCard.h"
 
+class GameController;
+
 class FormOfTheBatCard : public MonsterCard 
 {
 public:
     std::string get_name() const override;
     std::string get_description() const override;
 
-    void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice) override;
+    void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice , GameController& controller);
 };
