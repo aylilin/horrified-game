b3837a8d57a50447f6a27fa8afbfef13f3ddf53c
aylilin <aylinshahrokhi85@gmail.com>
2025-07-05 22:06:42 +0330

    some changes

--- include/map.h
+++ include/map.h
@@ -4,13 +4,20 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <unordered_map>
+#include <set>
+
 #include "item.h"
 #include "villager.h"
 #include "hero.h"
 #include "monster.h"
+#include "dice.h"
+#include "itemBag.h"
 
 class Monster;
 class Hero;
+class Villager;
+class GameController;
 
 class Map
 {
@@ -19,14 +26,16 @@ public:
     ~Map();
  
     void addLocation(const std::string& name);
-    void connectLocations(const std::string& from , const std::string& to);
     bool areConnected(const std::string& from , const std::string& to) const;
+    std::set<std::string> getConnections(const std::string& location) const;
+    void connectLocations(const std::string& from , const std::string& to);
 
     //for items
     void placeItem(const Item& item);
     const std::vector<Item>& get_itemsAt(const std::string& location) const;
     std::vector<Item>& get_itemsAt(const std::string& location); 
     ItemBag& get_itemBag();
+    const std::vector<Item>& get_getAllItems() const;
 
 
     //for monster and hero
@@ -36,17 +45,22 @@ public:
     //move on the map
     bool moveCharacter(const std::string& characterName , const std::string& toLocation);
     std::vector<Villager*> get_villagersAt(const std::string& location);
-    std::vector<Hero*> Map::getHeroesAt(const std::string& location);
+    std::vector<Hero*> getHeroesAt(const std::string& location);
 
     void placeHero(Hero* hero, const std::string& location);
+    std::string findNearestHeroLocation(const std::string& from , const std::vector<Hero*> heroes) const;
     void placeMonster(Monster* monster, const std::string& location);
-    void moveMonster(Monster* monster, const std::string& newLocation);
+    void moveMonster(Monster* monster, int steps , Dice& dice);
+    void monsterStrike(Monster* monster , int strikeCount , const std::vector<Hero*>& heroes , GameController& controller);
+    void manualMoveMonster(Monster* monster);
 
 
     std::vector<Villager*> getVillagersAt(const std::string& location);
     void markVillagerAsGuided(const std::string& name);
     void displayAvailableVillagers() const;
     void addVillager(Villager* v);
+    void setVillagerLocation(Villager* villager , const std::string& location);
+    void removeVillager(Villager* villager);
 
 
     void printMap() const;
@@ -54,11 +68,15 @@ public:
 
 private:
     std::map<std::string , std::set<std::string>> adjacencyList; 
+    std::map<std::string , std::set<std::string>> connections;
     std::map<std::string , std::vector<Item>> itemsInLocation;    
     std::map<std::string , std::string> characterPositions; 
     std::map<std::string, std::vector<Hero*>> heroPositions;
+    std::unordered_map<std::string , std::set<std::string>> locationConnections;
     std::vector<Villager*> villagers;
+    std::map<std::string , std::vector<Villager*>> villagerPositions;
     std::vector<Item> itemsOnMap;
+    std::vector<Item> items;
     ItemBag itemBag;
 
 };
