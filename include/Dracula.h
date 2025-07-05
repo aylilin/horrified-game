4a2cc88f9f2f849dafe313dcea51196960e14841
aylilin <aylinshahrokhi85@gmail.com>
2025-07-05 17:19:05 +0330

    adding 2 functions

--- include/Dracula.h
+++ include/Dracula.h
@@ -1,22 +1,22 @@
 #pragma once
 
 #include "monster.h"
+
 #include <map>
 
 class Dracula : public Monster
 {
 private:
     std::map<std::string , bool> coffins;
-    int coffinDestroyed;
+    int coffinDestroyed = 0;
 
 public:
     Dracula();
 
-    virtual int getCoffinsDestroyed() const;
+    int getCoffinsDestroyed() const;
     bool isCoffinDestroyed(const std::string& Location) const;
 
-    virtual void destroyCoffin(const std::string& Location);
-    virtual void useSpecialPower() override;
-    bool checkDefeatConditions() const override;
+    virtual void useSpecialPower();
+    bool checkDefeatConditions() const;
 
 };
