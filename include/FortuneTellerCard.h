abf9396c3da2674005645d492b816cb4530a11cb
aylilin <aylinshahrokhi85@gmail.com>
2025-07-05 17:24:08 +0330

    class GameController added

--- include/FortuneTellerCard.h
+++ include/FortuneTellerCard.h
@@ -1,11 +1,17 @@
 #pragma once
+
 #include "monsterCard.h"
 #include "itemBag.h"
+#include "map.h"
+#include "hero.h"
+#include "dice.h"
+
+class GameController;
 
 class FortuneTellerCard : public MonsterCard
 {
 public:
     std::string get_name() const override;
     std::string get_description() const override;
-    void apply(Map& map, std::vector<Monster*>& monsters, std::vector<Hero*>& heroes, Dice& dice) override;
+    void apply(Map& map, std::vector<Monster*>& monsters, std::vector<Hero*>& heroes, Dice& dice , GameController& controller);
 };
