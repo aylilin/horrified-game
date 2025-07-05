6db5aa52991feabff8a8e04680ebe1e710f0c039
aylilin <aylinshahrokhi85@gmail.com>
2025-07-05 22:36:22 +0330

    class GameController added

--- include/monsterCard.h
+++ include/monsterCard.h
@@ -1,11 +1,14 @@
 #pragma once
+
 #include <string>
 #include <vector>
 
-class Map;
+#include "map.h"
+
 class Monster;
 class Hero;
 class Dice;
+class GameController;
 
 class MonsterCard
 {
@@ -15,5 +18,5 @@ public:
     virtual std::string get_name() const = 0;
     virtual std::string get_description() const = 0;
 
-    virtual void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice) = 0;
+    virtual void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice , GameController& controller) = 0;
 };
