6784986bb5e0781efd5a256c89766f1916e20fb0
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 11:51:08 +0330

    add private data members to monster cards

--- include/EgyptianExpert.h
+++ include/EgyptianExpert.h
@@ -6,9 +6,17 @@ class GameController;
 
 class EgyptianExpertCard : public MonsterCard 
 {
+private:
+    static sf::Texture texture;
+    static bool texture_loaded;
+    
+    sf::Sprite sprite;
+
 public:
+    EgyptianExpertCard();
     std::string get_name() const override;
     std::string get_description() const override;
+    void show_monster_card() const;
 
     void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice , GameController& controller);
 };
