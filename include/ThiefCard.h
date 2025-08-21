c762868dd01d5854861c9885b38b58b0f9544051
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 18:23:00 +0330

    add private data members to monster cards

--- include/ThiefCard.h
+++ include/ThiefCard.h
@@ -4,9 +4,17 @@
 
 class ThiefCard : public MonsterCard
 {
+private:
+    static sf::Texture texture;
+    static bool texture_loaded;
+    
+    sf::Sprite sprite;
+
 public:
+    ThiefCard();
     std::string get_name() const override;
     std::string get_description() const override;
+    void show_monster_card() const;
 
     void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice , GameController& controller);
 };
