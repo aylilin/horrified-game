a835cca162b02bdd2b47f2599a8810da05878a1e
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 11:42:58 +0330

    update monster cards one by one

--- include/DeliveryCard.h
+++ include/DeliveryCard.h
@@ -5,9 +5,17 @@ class GameController;
 
 class DeliveryCard : public MonsterCard
 {
+private:
+    static sf::Texture texture;
+    static bool texture_loaded;
+    
+    sf::Sprite sprite;
+
 public:
+    DeliveryCard();
     std::string get_name() const override;
     std::string get_description() const override;
-
+    void show_monster_card() const;
+    
     void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice , GameController& controller);
 };
