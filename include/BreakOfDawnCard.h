a039ba8a7c8d2cd30fbc15920122a9fe76f2986c
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 10:47:18 +0330

    update monster cards one by one

--- include/BreakOfDawnCard.h
+++ include/BreakOfDawnCard.h
@@ -19,22 +19,21 @@ private:
     sf::Text descriptionText;
     sf::RectangleShape useButton;
     sf::Text buttonText;
-public:
-    // std::string get_name() const override;
-    // std::string get_description() const override;
 
+public:
+    std::string get_name() const override;
+    std::string get_description() const override;
+
+    BreakOfDawnCard();
     BreakOfDawnCard(sf::Font& font , const sf::Texture& texture);
 
-    void apply(Hero& currentHero , std::vector<Hero*>&allHeros , std::vector<Monster*>&monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase) override;
+    void apply(Hero& currentHero , std::vector<Hero*>& allHeros , std::vector<Monster*>& monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase) override;
 
-    std::string get_imagePath() const;
 
     bool handleClick(sf::Vector2f mousePos , Hero& currentHero , std::vector<Hero*>& allHeroes , std::vector<Monster*>& monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase);
 
     void render(sf::RenderWindow& window);
 
-    std::string getImagePath() const override {
-    return "assets/perkcards/break_of_dawn.png";
-}
+    std::string getImagePath() const override;
     
 };
