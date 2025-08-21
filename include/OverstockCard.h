06c6101e6c87cc7a21985bfaf2a8894cbb359968
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 18:02:57 +0330

    update

--- include/OverstockCard.h
+++ include/OverstockCard.h
@@ -15,23 +15,23 @@ class OverstockCard : public PerkCard
 public:
     OverstockCard();
 
-    std::string get_name() const override { return "Overstock"; }
-    std::string get_description() const override {
+    std::string get_name() const override 
+    { 
+        return "Overstock"; 
+    }
+
+    std::string get_description() const override 
+    {
         return "Each hero takes 1 item from the bag and places it in their current location.";
     }
 
-    void apply(Hero& currentHero,
-               std::vector<Hero*>& allHeroes,
-               std::vector<Monster*>& monsters , ItemBag& bag, Map& map, bool& skipMonsterPhase) override;
-
+    void apply(Hero& currentHero , std::vector<Hero*>& allHeroes , std::vector<Monster*>& monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase) override;
     void render(sf::RenderWindow& window);
-    void handleClick(sf::Vector2f mousePos, Hero& currentHero,
-                     std::vector<Hero*>& allHeroes,
-                     std::vector<Monster*>& monsters,
-                     ItemBag& bag, Map& map, bool& skipMonsterPhase);
+    void handleClick(sf::Vector2f mousePos , Hero& currentHero , std::vector<Hero*>& allHeroes , std::vector<Monster*>& monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase);
 
-    std::string getImagePath() const override {
-    return "assets/perkcards/break_of_dawn.png";
+    std::string getImagePath() const override 
+    {
+        return "../build/Perk_Cards/BreakOfDawn.png";
     }
 
 private:
