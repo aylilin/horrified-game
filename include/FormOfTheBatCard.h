feaddfceef92972ba8d49dac1ec3d132c6c7de52
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 11:55:43 +0330

    add private data members to monster cards

--- include/FormOfTheBatCard.h
+++ include/FormOfTheBatCard.h
@@ -1,13 +1,23 @@
 #pragma once
 #include "../include/monsterCard.h"
 
+#include <SFML/Graphics.hpp>
+
 class GameController;
 
 class FormOfTheBatCard : public MonsterCard 
 {
+private:
+    static sf::Texture texture;
+    static bool texture_loaded;
+    
+    sf::Sprite sprite;
+
 public:
+    FormOfTheBatCard();
     std::string get_name() const override;
     std::string get_description() const override;
+    void show_monster_card() const;
 
     void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice , GameController& controller);
 };
