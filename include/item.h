c79eb1988d6bcb13b525b944b7ad0894351735cb
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 17:25:13 +0330

    update constructors

--- include/item.h
+++ include/item.h
@@ -7,7 +7,10 @@ class Item
 {
 public:
     enum class Type {RED , BLUE , YELLOW};
-    Item(sf::RenderWindow& window , const sf::Sprite& mapSprite , const std::map<std::string, Position>& locations , std::map<Item::Type , sf::Texture>& itemTextures);
+
+    Item(const std::string& name , Type type , int power , const std::string& location);
+
+    Item(sf::RenderWindow& window , const sf::Sprite& mapSprite , const std::map<std::string , Position>& locations , std::map<Item::Type , sf::Texture>& itemTextures);
 
     Type get_type() const;
     int get_power() const;
@@ -17,18 +20,11 @@ public:
 
     std::string toString() const;  //for showing item info
 
-
-    const sf::Texture* getTexture() const;
-    const sf::Sprite& getSprite() const;
-
-    void setPosition(float x , float y);
-    void setScale(float scaleX , float scaleY);
-
 private:
     std::string name;
     Type type;
     int power;
-    std::string Location;
+    std::string location;
 
     sf::Texture texture;
     sf::Sprite sprite;
