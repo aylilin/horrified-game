15edad0db5fc2aa73a5cd8d2db454a10dd268a23
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 18:21:55 +0330

    add private data members to monster cards

--- include/TheInnocent.h
+++ include/TheInnocent.h
@@ -6,9 +6,17 @@ class GameController;
 
 class TheInnocentCard : public MonsterCard
 {
+private:
+    static sf::Texture texture;
+    static bool texture_loaded;
+    
+    sf::Sprite sprite;
+
 public:
+    TheInnocentCard();
     std::string get_name() const override;
     std::string get_description() const override;
+    void show_monster_card() const;
 
     void apply(Map& map , std::vector<Monster*>& monsters , std::vector<Hero*>& heroes , Dice& dice , GameController& controller);
 };
