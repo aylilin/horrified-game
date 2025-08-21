d21fdcccdf4c5879cad0bffca414d47a3c045a14
aylilin <aylinshahrokhi85@gmail.com>
2025-08-21 19:44:14 +0330

    add private data members to monster cards

--- src/DeliveryCard.cpp
+++ src/DeliveryCard.cpp
@@ -8,6 +8,29 @@
 
 #include <iostream>
 
+sf::Texture DeliveryCard::texture;
+bool DeliveryCard::texture_loaded = false;
+
+
+DeliveryCard::DeliveryCard()
+{
+    if (!texture_loaded)
+    {
+        if (texture.loadFromFile("../build/Monster_Cards/TheDelivery.png"))
+        {
+            texture_loaded = true;
+            std::cout << "Delivery card texture loaded successfully!" << std::endl;
+        } else {
+            std::cerr << "Failed to load delivery card texture!" << std::endl;
+        }
+    }
+    
+    if (texture_loaded)
+    {
+        sprite.setTexture(texture);
+    }
+}
+
 std::string DeliveryCard::get_name() const
 {
     return "Delivery";
@@ -61,9 +84,70 @@ void DeliveryCard::apply(Map& map , std::vector<Monster*>& monsters , std::vecto
         std::cout << "Invisible Man's power activated\n";
     }
 
-    Villager* v = new Villager("Unnamed", "Hospital");
+    Villager* v = new Villager("Unnamed" , "Hospital");
     v->set_location(invisibleMan->get_currentLocation());
     map.addVillager(v);
 
     std::cout << "there is one villager " << invisibleMan->get_currentLocation() << "\n";
+    this->show_monster_card();
+}
+
+void DeliveryCard::show_monster_card() const
+{
+    if (!texture_loaded)
+    {
+        std::cerr << "texture not loaded!" << std::endl;
+        return;
+    }
+
+    sf::RenderWindow window(sf::VideoMode(800 , 600) , "Delivery Card");
+    
+    sf::Font font;
+    if (!font.loadFromFile("ManufacturingConsent-Regular.ttf"))
+    {
+        std::cerr << "Failed to load font!" << std::endl;
+    }
+
+    sf::Text text;
+    text.setFont(font);
+    text.setFillColor(sf::Color::White);
+    sf::Sprite displaySprite = sprite;
+
+    displaySprite.setScale(0.2f , 0.2f);
+    displaySprite.setPosition(350 , 200);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+            
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(30 , 30 , 60));
+
+        window.draw(displaySprite);
+
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::White);
+        text.setString("Card Name: " + get_name());
+        text.setPosition(40 , 40);
+        window.draw(text);
+
+        text.setCharacterSize(20);
+        text.setFillColor(sf::Color::Yellow);
+        text.setString("Description: " + get_description());
+        text.setPosition(40 , 80);
+        window.draw(text);
+
+        window.display();
+    }
 }
