e37a3315e806350d8d1e07518757921c87e3cfdb
aylilin <aylinshahrokhi85@gmail.com>
2025-08-22 11:48:48 +0330

    update perk cards

--- src/RepelCard.cpp
+++ src/RepelCard.cpp
@@ -2,15 +2,18 @@
 #include <iostream>
 #include "dice.h"
 
-RepelCard::RepelCard() {
-    if (!texture.loadFromFile("assets/perk_cards/repel.png")) {
+RepelCard::RepelCard()
+{
+    if (!texture.loadFromFile("../build/Perk_Cards/Repel.png"))
+    {
         std::cerr << "Failed to load Repel card image!\n";
     }
     sprite.setTexture(texture);
-    sprite.setScale(0.5f, 0.5f);
-    sprite.setPosition(200, 100);
+    sprite.setScale(0.5f , 0.5f);
+    sprite.setPosition(200 , 100);
 
-    if (!font.loadFromFile("assets/fonts/arial.ttf")) {
+    if (!font.loadFromFile("../build/ManufacturingConsent-Regular.ttf"))
+    {
         std::cerr << "Failed to load font!\n";
     }
 
@@ -18,49 +21,49 @@ RepelCard::RepelCard() {
     descriptionText.setString(get_description());
     descriptionText.setCharacterSize(18);
     descriptionText.setFillColor(sf::Color::White);
-    descriptionText.setPosition(200, 320);
+    descriptionText.setPosition(200 , 320);
 
-    useButton.setSize(sf::Vector2f(120, 40));
-    useButton.setFillColor(sf::Color(50, 150, 50));
-    useButton.setPosition(250, 380);
+    useButton.setSize(sf::Vector2f(120 , 40));
+    useButton.setFillColor(sf::Color(50 , 150 , 50));
+    useButton.setPosition(250 , 380);
 
     buttonText.setFont(font);
     buttonText.setString("Use");
     buttonText.setCharacterSize(20);
     buttonText.setFillColor(sf::Color::White);
-    buttonText.setPosition(280, 385);
+    buttonText.setPosition(280 , 385);
 }
 
-void RepelCard::apply(Hero&, std::vector<Hero*>&,
-                      std::vector<Monster*>& monsters, ItemBag&,
-                      Map& map, bool&) {
+void RepelCard::apply(Hero& , std::vector<Hero*>& , std::vector<Monster*>& monsters , ItemBag& , Map& map , bool&) {
     Dice dice;
-    std::cout << "[Repel] Moving all monsters 2 steps.\n";
+    std::cout << "Move all monsters 2 steps.\n";
 
-    for (Monster* monster : monsters) {
-        if (monster) {
-            map.moveMonster(monster, 2, dice);
+    for (Monster* monster : monsters)
+    {
+        if (monster)
+        {
+            map.moveMonster(monster , 2 , dice);
             std::cout << monster->get_name() << " moved 2 steps.\n";
         }
     }
 }
 
-void RepelCard::draw(sf::RenderWindow& window) {
+void RepelCard::draw(sf::RenderWindow& window)
+{
     window.draw(sprite);
     window.draw(descriptionText);
     window.draw(useButton);
     window.draw(buttonText);
 }
 
-bool RepelCard::handleEvent(const sf::Event& event,
-                            Hero& currentHero, std::vector<Hero*>& allHeroes,
-                            std::vector<Monster*>& monsters, ItemBag& bag,
-                            Map& map, bool& skipMonsterPhase) {
-    if (event.type == sf::Event::MouseButtonPressed &&
-        event.mouseButton.button == sf::Mouse::Left) {
-        sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
-        if (useButton.getGlobalBounds().contains(mousePos)) {
-            apply(currentHero, allHeroes, monsters, bag, map, skipMonsterPhase);
+bool RepelCard::handleEvent(const sf::Event& event , Hero& currentHero , std::vector<Hero*>& allHeroes , std::vector<Monster*>& monsters , ItemBag& bag , Map& map , bool& skipMonsterPhase)
+{
+    if (event.type == sf::Event::MouseButtonPressed)
+    {
+        sf::Vector2f mousePos(event.mouseButton.x , event.mouseButton.y);
+        if (useButton.getGlobalBounds().contains(mousePos))
+        {
+            apply(currentHero , allHeroes , monsters , bag , map , skipMonsterPhase);
             return true;
         }
     }
