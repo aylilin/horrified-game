e54ad1ecbd2734fdad71172d7e5933cd11e057fc
aylilin <aylinshahrokhi85@gmail.com>
2025-08-22 11:53:47 +0330

    update monster cards

--- src/TheIchthyologist.cpp
+++ src/TheIchthyologist.cpp
@@ -4,6 +4,29 @@
 
 #include <iostream>
 
+sf::Texture TheIchthyologistCard::texture;
+bool TheIchthyologistCard::texture_loaded = false;
+
+TheIchthyologistCard::TheIchthyologistCard()
+{
+    if (!texture_loaded)
+    {
+        if (texture.loadFromFile("../build/Monster_Cards/TheIchtyologist.png"))
+        {
+            texture_loaded = true;
+            std::cout << "TheIchthyologist card texture loaded successfully!" << std::endl;
+        }
+        else{
+            std::cerr << "Failed to load TheIchthyologist card texture!" << std::endl;
+        }
+    }
+
+    if (texture_loaded)
+    {
+        sprite.setTexture(texture);
+    }
+}
+
 std::string TheIchthyologistCard::get_name() const
 {
     return "The Ichthyologist";
@@ -39,5 +62,66 @@ void TheIchthyologistCard::apply(Map& map , std::vector<Monster*>& monsters , st
             map.monsterStrike(m , strikes , heroes , controller);
             break;
         }
+    }
+    this->show_monster_card();
+
+}
+void TheIchthyologistCard::show_monster_card() const
+{
+    if (!texture_loaded)
+    {
+        std::cerr << "texture not loaded!" << std::endl;
+        return;
+    }
+
+    sf::RenderWindow window(sf::VideoMode(800 , 600) , "The Ichthyologist Card");
+    
+    sf::Font font;
+    if (!font.loadFromFile("ManufacturingConsent-Regular.ttf"))
+    {
+        std::cerr << "Failed to load font!" << std::endl;
+    }
+
+    sf::Text text;
+    text.setFont(font);
+    text.setFillColor(sf::Color::White);
+    
+    sf::Sprite displaySprite = sprite;
+    displaySprite.setScale(0.2f , 0.2f);
+    displaySprite.setPosition(350 , 200);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+            
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(30 , 30 , 60));
+
+        window.draw(displaySprite);
+
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::White);
+        text.setString("Card Name: " + get_name());
+        text.setPosition(40 , 40);
+        window.draw(text);
+
+        text.setCharacterSize(20);
+        text.setFillColor(sf::Color::Yellow);
+        text.setString("Description: " + get_description());
+        text.setPosition(40 , 80);
+        window.draw(text);
+
+        window.display();
     }
 }
